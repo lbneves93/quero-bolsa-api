29d64e4ec32bc0adab7373807e45f12bed974f50
Leonardo Neves <leo.b.neves93@gmail.com>
2021-01-28 13:52:11 -0300

    Adiciona model e tabela University

---
+++ db/migrate/20210127193423_create_universities.rb
@@ -0,0 +1,10 @@
+class CreateUniversities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :universities, id: :uuid do |t|
+      t.string :name, null: false
+      t.decimal :score, precision: 10, scale: 2, null: false
+      t.string :logo_url
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/universities.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :university do
+    name { FFaker::Education.school }
+    score { rand(1.0...5.0).round(2) }
+    logo_url { FFaker::Image.url }
+  end
+end
