03de31ed4c96ce8bf01287b0f690da9333ed2313
Leonardo Neves <leo.b.neves93@gmail.com>
2021-01-28 18:01:26 -0300

    Seeds de University,Campus,Course e Offer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless Rails.env.production?
+  p 'Creating Universities...'
+  universities = FactoryBot.create_list(:university, 10)
+
+  p 'Creating Campus...'
+  universities.each do |university|
+    FactoryBot.create_list(:campus, 3, university: university)
+  end
+
+  p 'Creating Courses...'
+  campus = Campus.all
+  courses = FactoryBot.create_list(:course, 30)
+  courses.each do |course|
+    course.campus << campus.sample(3)
+    course.save!
+  end
+
+  p 'Creating Offers...'
+  courses.each do |course|
+    course.campus.each do |campus|
+      FactoryBot.create(:offer, campus: campus, course: course)
+    end
+  end
+end
