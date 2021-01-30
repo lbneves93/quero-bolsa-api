ef9b732a3a30f3252f08e7bbd0fa36c3d71c5242
Leonardo Neves <leo.b.neves93@gmail.com>
2021-01-30 14:34:45 -0300

    Adiciona Titulo da API na pagina raiz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def home
+    render html: '<center><h1>Bem vindo a Quero Bolsa API!</h1></center>'.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'application#home'
   resources :courses, only: [:index]
   resources :offers, only: [:index]
 end
